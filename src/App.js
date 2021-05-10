1b7eb24ddb22eba03c17f3763ab0c8563e8937ad
Shruti Panjwani <67150258+shrutipanjwani@users.noreply.github.com>
2021-05-10 10:14:44 +0530

    day2 & day3

--- src/App.js
+++ src/App.js
@@ -1,36 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import landing from "./img/landing.jpg";
 
-function Header() {
+function Header(props) {
 	return (
 		<header>
-			<h1>Project Name</h1>
+			<h1>{props.name}</h1>
 		</header>
 	);
 }
 
-function Main() {
+function Main(props) {
 	return (
 		<header>
-			<h2>This is our project</h2>
+			<h2>This is our {props.type} project</h2>
+			<img src={landing} height={200} />
+			<ul style={{ textAlign: "left" }}>
+				{props.items.map((item) => (
+					<li key={item.id}>{item.title}</li>
+				))}
+			</ul>
 		</header>
 	);
 }
 
-function Footer() {
+function Footer(props) {
 	return (
 		<header>
-			<h3>All Rights Reserved Project Name &copy; 2021</h3>
+			<h3>All Rights Reserved Project Name &copy; {props.year}</h3>
 		</header>
 	);
+}
+
+// function Button() {
+// 	return (
+// 		<form onSubmit={handleSubmit}>
+// 			<button type="submit" onClick={() => console.log("Clicked!")}>
+// 				Say Clicked!!
+// 			</button>
+// 		</form>
+// 	);
+// }
+
+const [firstItem, ,] = ["lamp", "laptop", "phone"];
+
+console.log(firstItem);
+
+function Form() {
+	const [name, setName] = useState("");
+
+	function handleChange(e) {
+		setName(e.target.value);
+	}
+
+	function handleSubmit(e) {
+		e.preventDefault();
+		alert("Submitted");
+		setName("");
+	}
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<input type="text" value={name} required onChange={handleChange}></input>
+			<button type="submit">Submit</button>
+		</form>
+	);
+}
+
+const items = ["Books", "Pens", "Code", "Coffee"];
+
+const itemObjects = items.map((item, i) => ({ id: i, title: item }));
+
+function Regular() {
+	return <h1>Please Log in</h1>;
+}
+
+function Secret() {
+	return <h1>Welcome</h1>;
+}
+
+function Authorize({ authorized }) {
+	return <>{authorized ? <Secret /> : <Regular />}</>;
 }
 
 function App() {
 	return (
 		<div className="App">
-			<Header />
-			<Main />
-			<Footer />
+			<Authorize authorized={true} />
+			<Header name="React Practices" />
+			<Main type="react" items={itemObjects} />
+			<Footer year={new Date().getFullYear()} />
+			<Form />
+			{/* <Button /> */}
 		</div>
 	);
 }
